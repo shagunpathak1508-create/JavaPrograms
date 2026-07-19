aa9d1eeb50da2e099612a8577fe99e2338be9c7e
Your Name <your@email.com>
2026-07-19 19:06:57 +0530

    taking input from user

---
+++ Takeinput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Takeinput{
+  public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter first int: ");
+       int a=sc.nextInt();
+       System.out.println("Enter second int: ");
+       int b=sc.nextInt();
+       int sum=a+b;
+
+   System.out.println("Sum is: "+sum);
+
+}
+}
